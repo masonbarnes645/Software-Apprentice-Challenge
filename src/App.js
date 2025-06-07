13f802e9cf93e14ac0d7a977340fa9fc0bcdb3bf
Mason Barnes <barnesmason980@gmail.com>
2025-06-07 14:05:19 -0700

    useEffect for ad data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [advertisements, setAdvertisements] = useState([])
+  
+useEffect(() => {
+  fetch('http://localhost:3000/fakeDataSet')
+  .then((res) => {
+    if (res.ok) {
+      res.json().then(setAdvertisements)
+    } else {
+      throw new Error('Network response was not ok')
+    }
+  })
+  .catch((errObj) => console.error('Error:', errObj))
+  },[]) 
+
+  
   return (
     <div className="App">
       <header className="App-header">
